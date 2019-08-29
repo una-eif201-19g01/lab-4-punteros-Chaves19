efcc1577294690e49187dfd6b98faa1f5b9ea82b
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-29 17:44:27 -0600

    terminado

--- Empleado.cpp
+++ Empleado.cpp
@@ -14,58 +14,57 @@
  */
 
 
+#include <iostream>
 #include "Empleado.h"
 
-
-float Empleado::aumentoSalario(int *annosExperiencia) {
-    float salarioNuevo = 0;
-    if ((*annosExperiencia >= 1)&&(*annosExperiencia <= 3)) {
-        salarioNuevo = salarioBase * (salarioBase * 0.02);
+float Empleado::aumentoSalario(const int *annosExperiencia) {
+	float salarioNuevo = 0;
+    if ((*annosExperiencia >= 1) && (*annosExperiencia <= 3)) {
+        salarioNuevo = salarioBase + (salarioBase * 0.02);
     } else {
         if (*annosExperiencia > 3) {
-            salarioNuevo = salarioBase * (salarioBase * 0.05);
+            salarioNuevo = salarioBase + (salarioBase * 0.05);
         }
     }
     return salarioNuevo;
 }
 
 void Empleado::revisionAleatoria(bool *disponibilidad) {
-
+    if (rand() % 2 == 0){
+        *disponibilidad = 1;
+	} else{ *disponibilidad = 0;
+		}	
 }
 
-Empleado::Empleado(){
-	
-	
-	
+Empleado::Empleado() {
+	nombre = "";
+	annosExperiencia = 0;
+	salarioBase = 0;
+	identificador = std::rand() % LIMITESUPERIOR;
 }
 
-Empleado::Empleado(std::string nombre, int annosExperiencia, float salarioBase){
+Empleado::Empleado(const string *nombre, int annosExperiencia, float salarioBase) {
+    this->nombre = *nombre;
+    this->annosExperiencia = annosExperiencia;
+    this->salarioBase = salarioBase;
+    identificador = std::rand() % LIMITESUPERIOR;
+}
+
+string Empleado::obtenerDatos() {
 	
 	
-	
+    string reporte = "";
+    reporte = reporte + "Empleado [ " + std::to_string( getIdentificador() ) + " ]\nNombre [ " + getNombre() + " ]\nAnnos de experiencia [ " + std::to_string(annosExperiencia) + " ]\n"
+            "Salario Inicial [ " + std::to_string(salarioBase) + " ]\n"
+            "Salario Acumulado [ " + std::to_string(aumentoSalario(&annosExperiencia)) + " ]\n"
+            "Necesita revision [ "+ std::to_string(disponibilidad) + " ]\n";
+
+            return reporte;
 }
 
-std::string Empleado::reporteEmpleado() {
-
-    std::string reporte = "";
-    reporte = reporte + "Empleado # [" + std::to_string(identificador) + " ]\n\tNombre [ " + nombre + " ]\n\t"
-            "Annos de experiencia [" + std::to_string(annosExperiencia) + " ]\n\t"
-            "Salario Inicial [ " + std::to_string(salarioBase) + " ]\n\t"
-            "Salario Acumulado [ " + std::to_string(aumentoSalario(&annosExperiencia)) + " ]\n\t"
-            "Necesita revision [ " + std::to_string(disponibilidad) + " ]\n";
-	return reporte;
-}
-
-void Empleado::setDisponibilidad(bool disponibilidad) {
-    this->disponibilidad = disponibilidad;
-}
-
-bool Empleado::getDisponibilidad() {
-    return disponibilidad;
-}
 
 void Empleado::setSalarioBase(float salarioBase) {
-    this->salarioBase = salarioBase;
+	this->salarioBase = salarioBase;
 }
 
 float Empleado::getSalarioBase() {
@@ -80,7 +79,7 @@ int Empleado::getAnnosExperiencia() {
     return annosExperiencia;
 }
 
-void Empleado::setNombre(std::string nombre) {
+void Empleado::setNombre(string nombre) {
     this->nombre = nombre;
 }
 
@@ -89,7 +88,7 @@ std::string Empleado::getNombre() {
 }
 
 void Empleado::setIdentificador(int identificador) {
-    this->identificador = identificador;
+	this->identificador = identificador;
 }
 
 int Empleado::getIdentificador() {
