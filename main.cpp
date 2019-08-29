7d5c5c679d3220ef3828409fa5624afaabeea64f
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-29 17:23:19 -0600

    terminado

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Empleado.h
+ *
+ *    Description:  Definición del main
+ *
+ *        Created:  2019-08-22
+ *
+ *         Author:  Alonso Chaves achaqui@yahoo.com
+ *   Organization:  Universidad Nacional de Costa Rica
+ *
+ * =====================================================================================
+ */
+
+
 #include <iostream>
 #include "Empleado.h"
 
@@ -5,7 +21,7 @@ int main() {
     Empleado empleado[3];
 
     empleado[0].setNombre("Mike");
-    empleado[0].setAnoExperiencia(2);
+    empleado[0].setAnnosExperiencia(2);
     empleado[0].setSalarioBase(1200000);
 
     return 0;
