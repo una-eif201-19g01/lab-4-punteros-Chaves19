c17cf11d80b687a6c0599e5937a5dab49c7693f2
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-29 17:23:14 -0600

    terminado

--- Empleado.h
+++ Empleado.h
@@ -7,7 +7,7 @@
  *
  *        Created:  2019-08-22
  *
- *         Author:  Maikol Guzman Alan mikeguzman@gmail.com
+ *         Author:  Alonso Chaves achaqui@yahoo.com
  *   Organization:  Universidad Nacional de Costa Rica
  *
  * =====================================================================================
@@ -20,5 +20,43 @@
 
 #include <string>
 
+const int LIMITESUPERIOR = 100;
 
+
+class Empleado{
+protected:
+    int identificador;
+    std::string nombre;
+    int annosExperiencia;
+    int salarioBase;
+    bool disponibilidad;
+    float aumentoSalario(int *);
+    void revisionAleatoria(bool *);
+    
+public:
+    
+    Empleado();
+    Empleado(std::string, int, int);
+    std::string reporteEmpleado(std::string vector[]);
+    
+    void setDisponibilidad(bool);
+    bool getDisponibilidad();
+
+    void setSalarioBase(int);
+    int getSalarioBase();
+
+    void setAnnosExperiencia(int);
+    int getAnnosExperiencia();
+
+    void setNombre(std::string);
+    std::string getNombre();
+
+    void setIdentificador(int);
+    int getIdentificador();
+
+
+
+
+
+};
 #endif //EMPLEADOPUNTEROS_EMPLEADO_H
